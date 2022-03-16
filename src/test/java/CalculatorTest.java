d2496d26178339b0db71e7bf0d89572140126b0d
Domício Medeiros <domicioam@gmail.com>
2022-03-16 15:34:14 +0000

    refactor to use before each

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,12 +1,25 @@
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    Expression left;
+    Expression right;
+    Number a;
+    Number b;
+
+    @BeforeEach
+    public void setup() {
+         left = new Sum(new Number(1), new Number(2));
+         right = new Sum(new Number(4), new Number(5));
+         a = new Number(5);
+         b = new Number(6);
+    }
+
     @Test
     public void should_sum_two_numbers() {
         // Arrange
-        Number a = new Number(5);
-        Number b = new Number(6);
         Expression sum = new Sum(a, b);
 
         // Act
@@ -19,8 +32,6 @@ public class CalculatorTest {
     @Test
     public void should_sum_two_expressions() {
         // Arrange
-        Expression left = new Sum(new Number(1), new Number(2));
-        Expression right = new Sum(new Number(4), new Number(5));
         Expression sum = new Sum(left, right);
 
         // Act
@@ -33,8 +44,6 @@ public class CalculatorTest {
     @Test
     public void should_multiply_two_numbers() {
         // Arrange
-        Number a = new Number(5);
-        Number b = new Number(6);
         Expression multiply = new Multiply(a, b);
 
         // Act
@@ -47,8 +56,6 @@ public class CalculatorTest {
     @Test
     public void should_multiply_two_expressions() {
         // Arrange
-        Expression left = new Sum(new Number(1), new Number(2));
-        Expression right = new Sum(new Number(4), new Number(5));
         Expression multiply = new Multiply(left, right);
 
         // Act
